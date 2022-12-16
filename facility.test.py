a4986ec504d843adefb8e174549655205309d73e
chgordonlam <chinghingordon.lam@edu.sait.ca>
2022-12-15 20:26:36 -0700

    Update facility.test.py

--- facility.test.py
+++ facility.test.py
@@ -1,7 +1,7 @@
 # Student Name: Gordon Lam
 # Student ID: 000892800
 # Assignment: 4 - Classes (Individual)
-# Responsible for the facility class
+# Section responsible in the group project: facility class
 
 
 class facility:
@@ -25,21 +25,3 @@ class facility:
         appendFile.write("\n")
         appendFile.write(str(New_facility.FacilityName))
         appendFile.close()
-
-
-
-
-
-
-'''
-# The lines are used to test the methods (you may ignore them)
-print("Welcome to the AH Facility")
-option = int(input("Please choose the Facilities\n 1 - Display Facilities list \n 2 - Add Facility\n 3 - Back to the Main Menu\n"))
-
-if option == 1:
-    facility.displayFacilities(facility.displayFacilities)
-
-elif option == 2:
-    facility.writeListOffacilitiesToFile(facility.writeListOffacilitiesToFile)
-
-'''
